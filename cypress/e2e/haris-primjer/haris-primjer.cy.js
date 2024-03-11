a220fb46f9b9620f56f193f7d8c72c4295de73b9
HarisDedovic <dedovic.haris1@gmail.com>
2024-03-11 20:48:18 +0100

    halo

--- cypress/e2e/haris-primjer/haris-primjer.cy.js
+++ cypress/e2e/haris-primjer/haris-primjer.cy.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 1080,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here22y
+      // implement node event listeners here22ysafsdgsbdfgergenbfbfd
     },
   },
 });
