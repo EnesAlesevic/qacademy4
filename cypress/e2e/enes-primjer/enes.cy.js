bb90028e3738302c05c1eb3cba5c28649f47c657
enes.alesevic <enes.alesevic@symphony.is>
2024-03-11 20:33:30 +0100

    branch check

--- cypress/e2e/enes-primjer/enes.cy.js
+++ cypress/e2e/enes-primjer/enes.cy.js
@@ -2,4 +2,8 @@ describe('Test', ()=>{
     it('test001', ()=>{
         cy.visit('https://demo.nopcommerce.com')
     })
+    
+    it('test001', ()=>{
+        cy.visit('https://demo.nopcommerce.com')
+    })
 })
